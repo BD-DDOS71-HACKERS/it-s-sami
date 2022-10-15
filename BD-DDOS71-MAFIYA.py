73d0daaf2aa6a1772ae6dee50eec98c84f122b31
BD-DDOS71-HACKERS <115730542+BD-DDOS71-HACKERS@users.noreply.github.com>
2022-10-15 08:53:51 +0600

    Create BD-DDOS71-MAFIYA.py

---
+++ BD-DDOS71-MAFIYA.py
@@ -0,0 +1,43 @@
+import os
+
+logo = """\033[1;92m  ================================================
+
+ 
+
+ 
+
+██╗  ██╗     ███████╗ █████╗ ███╗   ███╗██╗
+
+╚██╗██╔╝     ██╔════╝██╔══██╗████╗ ████║██║
+
+ ╚███╔╝█████╗███████╗███████║██╔████╔██║██║
+
+ ██╔██╗╚════╝╚════██║██╔══██║██║╚██╔╝██║██║
+
+██╔╝ ██╗     ███████║██║  ██║██║ ╚═╝ ██║██║
+
+╚═╝  ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝
+
+                                                                                                                 
+
+================================================
+
+\033[93;1m   â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬â–¬ 
+
+ \033[1;32m[\033[0;41;37m==========={ AUTHER ðŸ”¥\033[0;37;41m it's sami }===========\033[1;37;40m\033[1;32m] \033[1;37;40m"""
+
+print (logo)
+
+os.system("xdg-open https://www.facebook.com/Hacker.of.ddos71.grop")
+
+virus = input ("\033[93;1mPress Enter For Virus")
+
+print("\033[93;1mhttps://drive.google.com/file/d/1Wm4VQ-HcPvjzsQyYXGR9W6s4flniiuJX/view?usp=drivesdk")
+
+print("\033[1;32mThis Is a Very Strong Virus Be Careful ")
+
+open = input("\033[1;32mPress Enter For Direct App link")
+
+os.system("xdg-open https://drive.google.com/file/d/1Wm4VQ-HcPvjzsQyYXGR9W6s4flniiuJX/view?usp=drivesdk")
+
+print(logo)
